d42cc5771241f079df7a5dbc4d0fb3f386b7cffa
Ian Maddison <ian@i.need.stronger.coffee>
2019-07-31 21:17:25 +0100

    Share uploader needs updating after new split between DHT and RPC side

--- src/main/java/com/goodforgoodbusiness/utils/ShareClient.java
+++ src/main/java/com/goodforgoodbusiness/utils/ShareClient.java
@@ -1,53 +1,53 @@
-package com.goodforgoodbusiness.utils;
-
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpRequest.BodyPublishers;
-import java.net.http.HttpResponse.BodyHandlers;
-
-import com.goodforgoodbusiness.model.TriTuple;
-import com.goodforgoodbusiness.shared.URIModifier;
-import com.goodforgoodbusiness.webapp.ContentType;
-
-public class ShareClient {
-	private static final HttpClient httpClient = HttpClient.newBuilder().build();
-	
-	public static void shareKeys(URI fromEndpoint, URI toEndpoint, TriTuple tt) throws Exception {		
-		// request a share key from Engine A
-		var shareRequestBuilder = URIModifier.from(fromEndpoint).appendPath("/share");
-		
-		if (tt.getSubject().isPresent()) shareRequestBuilder.addParam("sub", tt.getSubject().get());
-		if (tt.getPredicate().isPresent()) shareRequestBuilder.addParam("pre", tt.getPredicate().get());
-		if (tt.getObject().isPresent()) shareRequestBuilder.addParam("obj", tt.getObject().get());
-		
-		var shareRequest = HttpRequest
-			.newBuilder(shareRequestBuilder.build())
-			.header("Content-Type", ContentType.JSON.getContentTypeString())
-			.GET()
-			.build();
-		
-		System.out.println(shareRequest.uri());
-			
-		var shareRequestResponse = httpClient.send(shareRequest, BodyHandlers.ofString());
-		
-		if (shareRequestResponse.statusCode() != 200) {
-			throw new Exception("Response to request: " + shareRequestResponse.statusCode());
-		}
-		
-		// send it back in to Engine B
-		var shareAcceptBuilder = URIModifier.from(toEndpoint).appendPath("/share");
-		
-		var shareAcceptRequest = HttpRequest
-				.newBuilder(shareAcceptBuilder.build())
-				.header("Content-Type", ContentType.JSON.getContentTypeString())
-				.POST(BodyPublishers.ofString(shareRequestResponse.body()))
-				.build();
-			
-		var shareAcceptResponse = httpClient.send(shareAcceptRequest, BodyHandlers.ofString());
-		
-		if (shareAcceptResponse.statusCode() != 200) {
-			throw new Exception("Response to accept request: " + shareAcceptResponse.statusCode());
-		}
-	}
-}
+//package com.goodforgoodbusiness.utils;
+//
+//import java.net.URI;
+//import java.net.http.HttpClient;
+//import java.net.http.HttpRequest;
+//import java.net.http.HttpRequest.BodyPublishers;
+//import java.net.http.HttpResponse.BodyHandlers;
+//
+//import com.goodforgoodbusiness.model.TriTuple;
+//import com.goodforgoodbusiness.shared.URIModifier;
+//import com.goodforgoodbusiness.webapp.ContentType;
+//
+//public class ShareClient {
+//	private static final HttpClient httpClient = HttpClient.newBuilder().build();
+//	
+//	public static void shareKeys(URI fromEndpoint, URI toEndpoint, TriTuple tt) throws Exception {		
+//		// request a share key from Engine A
+//		var shareRequestBuilder = URIModifier.from(fromEndpoint).appendPath("/share");
+//		
+//		if (tt.getSubject().isPresent()) shareRequestBuilder.addParam("sub", tt.getSubject().get());
+//		if (tt.getPredicate().isPresent()) shareRequestBuilder.addParam("pre", tt.getPredicate().get());
+//		if (tt.getObject().isPresent()) shareRequestBuilder.addParam("obj", tt.getObject().get());
+//		
+//		var shareRequest = HttpRequest
+//			.newBuilder(shareRequestBuilder.build())
+//			.header("Content-Type", ContentType.JSON.getContentTypeString())
+//			.GET()
+//			.build();
+//		
+//		System.out.println(shareRequest.uri());
+//			
+//		var shareRequestResponse = httpClient.send(shareRequest, BodyHandlers.ofString());
+//		
+//		if (shareRequestResponse.statusCode() != 200) {
+//			throw new Exception("Response to request: " + shareRequestResponse.statusCode());
+//		}
+//		
+//		// send it back in to Engine B
+//		var shareAcceptBuilder = URIModifier.from(toEndpoint).appendPath("/share");
+//		
+//		var shareAcceptRequest = HttpRequest
+//				.newBuilder(shareAcceptBuilder.build())
+//				.header("Content-Type", ContentType.JSON.getContentTypeString())
+//				.POST(BodyPublishers.ofString(shareRequestResponse.body()))
+//				.build();
+//			
+//		var shareAcceptResponse = httpClient.send(shareAcceptRequest, BodyHandlers.ofString());
+//		
+//		if (shareAcceptResponse.statusCode() != 200) {
+//			throw new Exception("Response to accept request: " + shareAcceptResponse.statusCode());
+//		}
+//	}
+//}
--- src/main/java/com/goodforgoodbusiness/utils/ingest/ShareUploader.java
+++ src/main/java/com/goodforgoodbusiness/utils/ingest/ShareUploader.java
@@ -1,66 +1,66 @@
-package com.goodforgoodbusiness.utils.ingest;
-
-import static com.goodforgoodbusiness.utils.ShareClient.shareKeys;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URI;
-import java.util.Optional;
-
-import com.goodforgoodbusiness.model.TriTuple;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-
-/** 
- * Reads a series of share keys in Joe's shares.json format
- */
-public abstract class ShareUploader {
-	private final JsonArray shares;
-	
-	protected ShareUploader(File shareFile) throws FileNotFoundException, IOException {
-		if (shareFile.exists() && shareFile.isFile()) {
-			try (var fp = new FileReader(shareFile)) {
-				this.shares = new JsonParser().parse(fp).getAsJsonArray();
-			}
-		}
-		else {
-			throw new IllegalArgumentException(shareFile.getAbsolutePath());
-		}
-	}
-	
-	protected abstract URI getToEngine(String infoCreator);
-	protected abstract URI getFromEngine(String infoCreator);
-	
-	protected void run(boolean startOnly) throws Exception {
-		for (var i = 0; i < shares.size(); i++) {
-			var shareStmt = shares.get(i).getAsJsonObject();
-			
-			var infoCreator = shareStmt.get("information_creator").getAsString();
-			var patternsArray = shareStmt.get("patterns").getAsJsonArray();
-			var includeAtStart = shareStmt.get("include_at_start").getAsBoolean();
-			
-//			if ((includeAtStart && startOnly) || !includeAtStart) {
-				var shareToUri = getToEngine(infoCreator);
-				var shareFromUri = getFromEngine(infoCreator);
-				
-				if (shareFromUri != null) {
-					for (var j = 0; j < patternsArray.size(); j++) {
-						var pattern = patternsArray.get(j).getAsJsonObject();
-						var spec = 
-							TriTuple.fromN3Quoted(
-								Optional.ofNullable(pattern.get("s")).map(JsonElement::getAsString),
-								Optional.ofNullable(pattern.get("p")).map(JsonElement::getAsString),
-								Optional.ofNullable(pattern.get("o")).map(JsonElement::getAsString)
-							)
-						;
-						
-						shareKeys(shareFromUri, shareToUri, spec);
-					}
-				}
+//package com.goodforgoodbusiness.utils.ingest;
+//
+//import static com.goodforgoodbusiness.utils.ShareClient.shareKeys;
+//
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.net.URI;
+//import java.util.Optional;
+//
+//import com.goodforgoodbusiness.model.TriTuple;
+//import com.google.gson.JsonArray;
+//import com.google.gson.JsonElement;
+//import com.google.gson.JsonParser;
+//
+///** 
+// * Reads a series of share keys in Joe's shares.json format
+// */
+//public abstract class ShareUploader {
+//	private final JsonArray shares;
+//	
+//	protected ShareUploader(File shareFile) throws FileNotFoundException, IOException {
+//		if (shareFile.exists() && shareFile.isFile()) {
+//			try (var fp = new FileReader(shareFile)) {
+//				this.shares = new JsonParser().parse(fp).getAsJsonArray();
 //			}
-		}
-	}
-}
+//		}
+//		else {
+//			throw new IllegalArgumentException(shareFile.getAbsolutePath());
+//		}
+//	}
+//	
+//	protected abstract URI getToEngine(String infoCreator);
+//	protected abstract URI getFromEngine(String infoCreator);
+//	
+//	protected void run(boolean startOnly) throws Exception {
+//		for (var i = 0; i < shares.size(); i++) {
+//			var shareStmt = shares.get(i).getAsJsonObject();
+//			
+//			var infoCreator = shareStmt.get("information_creator").getAsString();
+//			var patternsArray = shareStmt.get("patterns").getAsJsonArray();
+//			var includeAtStart = shareStmt.get("include_at_start").getAsBoolean();
+//			
+////			if ((includeAtStart && startOnly) || !includeAtStart) {
+//				var shareToUri = getToEngine(infoCreator);
+//				var shareFromUri = getFromEngine(infoCreator);
+//				
+//				if (shareFromUri != null) {
+//					for (var j = 0; j < patternsArray.size(); j++) {
+//						var pattern = patternsArray.get(j).getAsJsonObject();
+//						var spec = 
+//							TriTuple.fromN3Quoted(
+//								Optional.ofNullable(pattern.get("s")).map(JsonElement::getAsString),
+//								Optional.ofNullable(pattern.get("p")).map(JsonElement::getAsString),
+//								Optional.ofNullable(pattern.get("o")).map(JsonElement::getAsString)
+//							)
+//						;
+//						
+//						shareKeys(shareFromUri, shareToUri, spec);
+//					}
+//				}
+////			}
+//		}
+//	}
+//}
